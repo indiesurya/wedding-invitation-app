e81ab93c3aa60daf77c13213d0464d3ea12bcc27
indiesurya <putuindie11@gmail.com>
2022-04-24 17:36:31 +0800

    fix bug

--- resources/views/_includes/head.blade.php
+++ resources/views/_includes/head.blade.php
@@ -11,8 +11,8 @@
 <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
 <!-- Custom styles for this template-->
-<link href="{{ assets('sbadmin/css/sb-admin-2.min.css') }}" rel="stylesheet">
-<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<link href="sbadmin/css/sb-admin-2.min.css" rel="stylesheet">
+<link rel="stylesheet" href="css/app.css">
 
 @stack('css')
 
